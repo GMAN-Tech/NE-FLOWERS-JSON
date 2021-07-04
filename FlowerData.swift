1ac73953af8d9f6c0a07f6adc601b16b56e78a67
GMAN-Tech <84152780+GMAN-Tech@users.noreply.github.com>
2021-07-04 12:48:17 -0400

    Add files via upload

---
+++ FlowerData.swift
@@ -0,0 +1,39 @@
+//
+//  FlowerData.swift
+//  New England Flowers
+//
+//  Created by Gary on 7/3/21.
+//
+
+import SwiftUI
+
+let flowerData: [Flower] = [
+   Flower(
+      title: "Day Lily",
+      img: "1"
+   ),
+   Flower(
+      title: "Daisy",
+      img: "2"
+   ),
+   Flower(
+      title: "Cosmo",
+      img: "3"
+   ),
+   Flower(
+      title: "Pansies",
+      img: "4"
+   ),
+   Flower(
+      title: "Poppies",
+      img: "5"
+   ),
+   Flower(
+      title: "Straw Flowers",
+      img: "6"
+   ),
+   Flower(
+      title: "Tiger Lily",
+      img: "7"
+   )
+]
